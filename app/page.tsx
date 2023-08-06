deb1a809c0e60482486408613dcc779e4d6e768c
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2023-08-06 22:05:26 +0200

    update: search section is added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,49 @@
-import Hero from '@/components/Hero'
-import Image from 'next/image'
+import CarCard from "@/components/CarCard";
+import CustomFilter from "@/components/CustomFilter";
+import Hero from "@/components/Hero";
+import SearchBar from "@/components/SearchBar";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
-     <Hero />
+      <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Lutfullah Car Cataloge</h1>
+          <p>Explore the cars you like at Lutfullah CarHub</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">
+              Oops, No Car in Lutfullah CarHub
+            </h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
     </main>
-  )
+  );
 }
