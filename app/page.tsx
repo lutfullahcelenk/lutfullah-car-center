e6533f046dc38b456d5322e1b11734fa11332de2
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2023-08-07 00:06:45 +0200

    fix: data fetching parameter is fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,48 +2,58 @@ import CarCard from "@/components/CarCard";
 import CustomFilter from "@/components/CustomFilter";
 import Hero from "@/components/Hero";
 import SearchBar from "@/components/SearchBar";
+import { HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
 
-export default async function Home() {
-  const allCars = await fetchCars();
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+export default async function Home({ searchParams }: HomeProps) {
+    const allCars = await fetchCars({
+        manufacturer: searchParams.manufacturer || "",
+        year: searchParams.year || 2022,
+        fuel: searchParams.fuel || "",
+        limit: searchParams.limit || 10,
+        model: searchParams.model || "",
+    });
+    const isDataEmpty =
+        !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
-  return (
-    <main className="overflow-hidden">
-      <Hero />
+    return (
+        <main className="overflow-hidden">
+            <Hero />
 
-      <div className="mt-12 padding-x padding-y max-width" id="discover">
-        <div className="home__text-container">
-          <h1 className="text-4xl font-extrabold">Lutfullah Car Cataloge</h1>
-          <p>Explore the cars you like at Lutfullah CarHub</p>
-        </div>
+            <div className="mt-12 padding-x padding-y max-width" id="discover">
+                <div className="home__text-container">
+                    <h1 className="text-4xl font-extrabold">
+                        Lutfullah Car Cataloge
+                    </h1>
+                    <p>Explore the cars you like at Lutfullah CarHub</p>
+                </div>
 
-        <div className="home__filters">
-          <SearchBar />
+                <div className="home__filters">
+                    <SearchBar />
 
-          <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
-          </div>
-        </div>
+                    <div className="home__filter-container">
+                        <CustomFilter title="fuel" />
+                        <CustomFilter title="year" />
+                    </div>
+                </div>
 
-        {!isDataEmpty ? (
-          <section>
-            <div className="home__cars-wrapper">
-              {allCars.map((car) => (
-                <CarCard car={car} />
-              ))}
+                {!isDataEmpty ? (
+                    <section>
+                        <div className="home__cars-wrapper">
+                            {allCars.map((car) => (
+                                <CarCard car={car} />
+                            ))}
+                        </div>
+                    </section>
+                ) : (
+                    <div className="home__error-container">
+                        <h2 className="text-black text-xl font-bold">
+                            Oops, No Car in Lutfullah CarHub
+                        </h2>
+                        <p>{allCars?.message}</p>
+                    </div>
+                )}
             </div>
-          </section>
-        ) : (
-          <div className="home__error-container">
-            <h2 className="text-black text-xl font-bold">
-              Oops, No Car in Lutfullah CarHub
-            </h2>
-            <p>{allCars?.message}</p>
-          </div>
-        )}
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
--- types/index.ts
+++ types/index.ts
@@ -1,40 +1,52 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-  title: string;
-  containerStyles?: string;
-  btnType?: "button" | "submit";
-  textStyles?: string;
-  rightIcon?: string;
-  handleClick?: MouseEventHandler<HTMLButtonElement>;
+    title: string;
+    containerStyles?: string;
+    btnType?: "button" | "submit";
+    textStyles?: string;
+    rightIcon?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface SearchManufacurerProps {
-  manufacturer: string;
-  setManufacturer: (manufacturer: string) => void;
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
 }
 
 export interface CarProps {
-  city_mpg: number;
-  class: string;
-  combination_mpg: number;
-  cylinders: number;
-  displacement: number;
-  drive: string;
-  fuel_type: string;
-  highway_mpg: number;
-  make: string;
-  model: string;
-  transmission: string;
-  year: number;
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
 }
 
 export interface CarCardProps {
-  model: string;
-  make: string;
-  mpg: number;
-  transmission: string;
-  year: number;
-  drive: string;
-  cityMPG: number;
+    model: string;
+    make: string;
+    mpg: number;
+    transmission: string;
+    year: number;
+    drive: string;
+    cityMPG: number;
+}
+
+export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+}
+
+export interface HomeProps {
+    searchParams: FilterProps;
 }
