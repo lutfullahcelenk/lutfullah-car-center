8407f901bf3210370bfecfb8d641a75bcf3e9605
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2023-08-06 22:05:45 +0200

    update: new types are added to ButtonProps

--- components/CustomButton/index.tsx
+++ components/CustomButton/index.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import { CustomButtonProps } from "@/types";
+import Image from "next/image";
 
 const CustomButton = ({
   title,
   containerStyles,
   btnType,
+  textStyles,
+  rightIcon,
   handleClick,
 }: CustomButtonProps) => {
   return (
@@ -17,7 +19,18 @@ const CustomButton = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="arrow_left"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
--- types/index.ts
+++ types/index.ts
@@ -4,5 +4,37 @@ export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManufacurerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface CarCardProps {
+  model: string;
+  make: string;
+  mpg: number;
+  transmission: string;
+  year: number;
+  drive: string;
+  cityMPG: number;
 }
